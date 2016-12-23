2f934eb53b3a1f610725719ff9e9ac4205ac4c90
aikovalenko <kovalenko.ai@yandex.ru>
2016-12-24 01:20:07 +0300

    first slide responsive

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,21 +1,8 @@
 //= ../libs/jquery/dist/jquery.min.js
+
 //= ../libs/fullpage.js/dist/scrolloverflow.js
 //= ../libs/fullpage.js/dist/jquery.fullpage.min.js
 //= ../libs/modernizr/modernizr-custom.js
 //= ../libs/fastclick/lib/fastclick.js
 //= ../libs/platform.js
-
-function truncate() {
-    var truncate = $('p.truncate');
-
-    truncate.each(function(){
-        var text = $(this).text(),
-            textLength = 254;
-        if(text.length < textLength) return;
-
-        $(this).html(
-            text.slice(0,textLength) + '<span>... </span>'
-        );
-
-    });
-}
+//= ../libs/color-thief.min.js
