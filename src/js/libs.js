52867a810c0b3e747e65520ef383d4bbe1f91be6
aikovalenko <kovalenko.ai@yandex.ru>
2017-01-11 19:27:16 +0300

    first slide responsive

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,8 +1,8 @@
 //= ../libs/jquery/dist/jquery.min.js
-
 //= ../libs/fullpage.js/dist/scrolloverflow.js
 //= ../libs/fullpage.js/dist/jquery.fullpage.min.js
 //= ../libs/modernizr/modernizr-custom.js
 //= ../libs/fastclick/lib/fastclick.js
 //= ../libs/platform.js
 //= ../libs/color-thief.min.js
+//= ../libs/slick-carousel/slick/slick.min.js
